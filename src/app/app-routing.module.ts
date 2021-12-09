19efd11a30ed3d0a8b2a1bff147e1fc243f0aba6
unknown <laderer5@gmail.com>
2021-12-09 14:36:45 -0500

    25

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
